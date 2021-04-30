e43b5f7be54a9641ee5ac46d0ed4753bc9d00637
Matthijs Kropholler <matthijsk2000@pm.me>
2021-04-30 11:37:28 +0200

    Add puzzles

--- Sudoku/Domain/Parsers/SudokuReader.cs
+++ Sudoku/Domain/Parsers/SudokuReader.cs
@@ -8,11 +8,18 @@ namespace Sudoku.Domain.Parsers
 {
     public class SudokuReader
     {
-        private SudokuFactory _sudokuFactory;
+        private SudokuFactory _sudokuFactory = new SudokuFactory();
+
+        SudokuReader()
+        {
+            _sudokuFactory.RegisterType(".4x4", typeof(NormalSudokuParserFactory));
+        }
 
         public Game Read(string path)
         {
             throw new System.NotImplementedException();
+
+            // Factory stuff
         }
     }
 }
