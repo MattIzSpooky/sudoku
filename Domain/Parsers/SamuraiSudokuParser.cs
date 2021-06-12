d6d33f3e1440cdc4764dabcc52c611b7e9ad6b62
Matthijs Kropholler <matthijsk2000@pm.me>
2021-06-12 13:40:04 +0200

    Add SamuraiSudokuParser tests

--- Domain/Parsers/SamuraiSudokuParser.cs
+++ Domain/Parsers/SamuraiSudokuParser.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
+using System.Text.RegularExpressions;
 using Domain.Board;
 using Domain.Board.Leaves;
 
@@ -10,18 +11,20 @@ namespace Domain.Parsers
     public class SamuraiSudokuParser : NormalSudokuParser
     {
         private const int AmountOfSubSudoku = 5;
-        
-        private static readonly int[] XOffsets = {
+
+        private static readonly int[] XOffsets =
+        {
             0, 16, 8, 0, 16
         };
 
-        private static readonly int[] YOffsets = {
+        private static readonly int[] YOffsets =
+        {
             0, 0, 8, 16, 16
         };
 
         public override Field[] Parse(string content, int offsetX = 0, int offsetY = 0)
         {
-            var cleanedContent = content.Replace(Environment.NewLine, string.Empty).Trim();
+            var cleanedContent = Regex.Replace(content.Replace(Environment.NewLine, string.Empty).Trim(), @"\s+", "");
             var amountOfCellsPerSubSudoku = cleanedContent.Length / AmountOfSubSudoku;
 
             var grids = Enumerable
@@ -30,7 +33,7 @@ namespace Domain.Parsers
                 .SelectMany((subSudokuContent, i) => base.Parse(subSudokuContent, XOffsets[i], YOffsets[i]))
                 .ToArray();
 
-            
+
             MergeOverflowingQuadrants(grids, amountOfCellsPerSubSudoku);
 
             return grids;
@@ -39,17 +42,18 @@ namespace Domain.Parsers
         private static void MergeOverflowingQuadrants(IReadOnlyList<Field> grids, int amountOfCellsPerSubSudoku)
         {
             const int quadrantFactor = 2;
-            
+
             var centerQuadrant = 0;
             var centerGrid = grids.Count / quadrantFactor;
             var leftQuadrant = (int) Math.Sqrt(amountOfCellsPerSubSudoku) - 1;
-            
+
             for (var i = 0; i < grids.Count; i++)
             {
                 if (i != centerGrid)
                 {
                     var firstQuadrant = grids[i].Quadrants[leftQuadrant].Children.OfType<CellLeaf>().ToImmutableList();
-                    var secondQuadrant = grids[centerGrid].Quadrants[centerQuadrant].Children.OfType<CellLeaf>().ToImmutableList();
+                    var secondQuadrant = grids[centerGrid].Quadrants[centerQuadrant].Children.OfType<CellLeaf>()
+                        .ToImmutableList();
 
                     MergeOverflowingCells(firstQuadrant, secondQuadrant);
                 }
@@ -60,7 +64,8 @@ namespace Domain.Parsers
             }
         }
 
-        private static void MergeOverflowingCells(IReadOnlyList<CellLeaf> firstCells, IReadOnlyList<CellLeaf> secondCells)
+        private static void MergeOverflowingCells(IReadOnlyList<CellLeaf> firstCells,
+            IReadOnlyList<CellLeaf> secondCells)
         {
             for (var i = 0; i < firstCells.Count; i++)
                 firstCells[i].Value = secondCells[i].Value;
---
+++ Tests/ISudokuParser/SamuraiSudokuParserTests.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Linq;
+using Domain.Board;
+using Domain.Board.Leaves;
+using Domain.Parsers;
+using NUnit.Framework;
+
+namespace Tests.ISudokuParser
+{
+    public class SamuraiSudokuParserTests
+    {
+        private Field[] _fields;
+
+        private const int AmountOfFields = 5;
+
+        private const string SamuraiSudoku =
+            @"800000700003050206700300095000091840000007002000062000000000000609080000002903000
+            149000000000091000000060000007120008000000340405008067000000000000007020000050003
+            000000000000008000000004000010600005030070080800005010000900000000800000000000000
+            900060000030400000000000000390800407065000000200037600000080000000190000000000914
+            000402800000080902000000000000610000400800000098750000670008001901060700002000009";
+
+        [SetUp]
+        public void Setup()
+        {
+            _fields = new SamuraiSudokuParser().Parse(SamuraiSudoku);
+        }
+
+        [Test]
+        public void Parse_WithSamurai_ShouldReturnFiveFields()
+        {
+            Assert.True(_fields.Length == AmountOfFields);
+        }
+
+        [Test]
+        public void Parse_WithSamurai_ShouldHaveNineQuadrantsPerField()
+        {
+            foreach (var field in _fields)
+            {
+                Assert.AreEqual(9, field.Quadrants.Count);
+            }
+        }
+
+        [Test]
+        public void Parse_WithSamurai_EachQuadrantHasOffsets()
+        {
+            var firstCellFirstQuadrant = _fields[0].Quadrants[0].Cells[0];
+            var firstCellSecondQuadrant = _fields[1].Quadrants[0].Cells[0];
+
+            Assert.AreNotEqual(firstCellFirstQuadrant.Coordinate, firstCellSecondQuadrant.Coordinate);
+        }
+
+        [Test]
+        public void Parse_WithSamurai_ShouldHaveOverflowingCells()
+        {
+            const int quadrantFactor = 2;
+
+            var centerQuadrant = 0;
+            var centerGrid = _fields.Length / quadrantFactor;
+            var leftQuadrant = 8;
+
+            for (var i = 0; i < _fields.Length; i++)
+            {
+                if (i != centerGrid)
+                {
+                    var firstFieldLastQuadrant = _fields[i].Quadrants[leftQuadrant];
+                    var centerFieldLastQuadrant = _fields[centerGrid].Quadrants[centerQuadrant];
+
+                    for (var j = 0; j < firstFieldLastQuadrant.Cells.Count; j++)
+                    {
+                        var cellFirstField = firstFieldLastQuadrant.Cells[j];
+                        var cellCenterField = centerFieldLastQuadrant.Cells[j];
+                
+                        Assert.AreSame(cellFirstField.Value, cellCenterField.Value);
+                    }
+                }
+
+
+                leftQuadrant -= quadrantFactor;
+                centerQuadrant += quadrantFactor;
+            }
+        }
+    }
+}
